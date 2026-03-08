b9b84e4bb631fbbc0fce0ed4bf08eb337646d6db
unknown <yafialif01@gmail.com>
2026-03-08 21:45:44 +0700

    first commit

---
+++ includes/class-menu.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ContentAplikasi;
+
+class Menu
+{
+    const MENU_SLUG = 'content-aplikasi';
+
+    public function __construct()
+    {
+        add_action('admin_menu', [$this, 'register_menu']);
+    }
+     public function register_menu()
+    {
+                // MENU UTAMA
+        add_menu_page(
+            'Content Aplikasi',          // Page title
+            'Content Aplikasi',          // Menu title
+            'manage_options',            // Capability
+            self::MENU_SLUG,              // Menu slug
+            [$this, 'dashboard'],         // Callback
+            'dashicons-admin-multisite', // Icon
+            5                             // Position
+        );
+
+
+
+
+    public function dashboard()
+        {
+    ?>
+            <div class="wrap">
+                <h1>Content Aplikasi</h1>
+                <p>Kelola konten Event, HF Clinic, News, dan Recording melalui menu di samping.</p>
+            </div>
+    <?php
+    }
+    }
+}
---
+++ includes/class-plugin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ContentAplikasi;
+
+// if (!defined('ABSPATH')) exit;
+// register_activation_hook(__FILE__, function() {
+//     \ContentAplikasi\App_Table::create_table();
+// });
+class Plugin
+{
+
+    private static $instance = null;
+
+    public static function instance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        $this->includes();
+        $this->init();
+    }
+
+    private function includes()
+    {
+        require_once CA_PATH . 'includes/class-menu.php';
+        require_once CA_PATH . 'includes/class-cpt.php';
+        require_once CA_PATH . 'includes/class-meta.php';
+        require_once CA_PATH . 'includes/class-rest-api.php';
+        
+
+    }
+
+    private function init()
+    {
+        new Menu();
+        new CPT();
+        new Meta();
+        new RestAPI();
+
+    }
+    
+}

538246da2bb04c7d6c2974f85f179a3771a38d46
unknown <yafialif01@gmail.com>
2026-04-05 00:32:57 +0700

    Update Agenda

--- includes/class-rest-api.php
+++ includes/class-rest-api.php
@@ -103,7 +103,7 @@ class RestAPI
                     [
                         'taxonomy' => 'category',
                         'field'    => 'term_id',
-                        'terms'    => $term['term_id'],
+                        'terms'    => $term->term_id,
                     ]
                 ]
             ]);
@@ -121,7 +121,7 @@ class RestAPI
             }
 
             $result[] = [
-                'group_title' => $term['name'],
+                'group_title' => $term->name,
                 'group_items' => $items
             ];
         }

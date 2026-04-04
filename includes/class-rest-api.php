b26ed5b76572f1c4ad03c8e05c95f056fc550e3c
unknown <yafialif01@gmail.com>
2026-04-04 13:36:14 +0700

    handle Checkin

--- includes/class-rest-api.php
+++ includes/class-rest-api.php
@@ -182,7 +182,6 @@ class RestAPI
                     'prefix'     => 'Visited',
                     'suffix'       => '<p color=\"green\"><b>Done</b></p>'
                 ];
-            }
 
 
         return [
